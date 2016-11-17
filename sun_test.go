c8f4f32d24a8879147dd947cdb8b7569524f2576
Dimitri Sokolyuk <ds@doozer.de>
2016-11-17 19:43:16 +0100

    Be less global

--- sun_test.go
+++ sun_test.go
@@ -6,39 +6,56 @@ import (
 )
 
 const (
-	lat = 52.5200
-	lon = 13.4050
-)
-
-var (
-	when, _ = time.Parse(time.RFC3339, "2009-11-10T23:00:00Z")
-	rise, _ = time.Parse(time.RFC3339, "2009-11-10T06:18:22Z")
-	set, _  = time.Parse(time.RFC3339, "2009-11-10T15:21:24Z")
+	lat  = 52.5200
+	lon  = 13.4050
+	when = "2009-11-10T23:00:00Z"
+	rise = "2009-11-10T06:18:22Z"
+	set  = "2009-11-10T15:21:24Z"
 )
 
 func TestDayOfYear(t *testing.T) {
-	d := dayOfYear(when)
+	date, err := time.Parse(time.RFC3339, when)
+	if err != nil {
+		t.Fatal(err)
+	}
+	d := dayOfYear(date)
 	if d != 314 {
 		t.Errorf("expected 314, got %v", d)
 	}
 }
 
 func TestRise(t *testing.T) {
-	r, err := Rise(when, lat, lon)
+	date, err := time.Parse(time.RFC3339, when)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, err := Rise(date, lat, lon)
 	if err != nil {
 		t.Error(err)
 	}
-	if r.Truncate(time.Second) != rise {
-		t.Errorf("expected %v, got %v", rise, r)
+	sr, err := time.Parse(time.RFC3339, rise)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r.Truncate(time.Second) != sr {
+		t.Errorf("expected %v, got %v", rise, sr)
 	}
 }
 
 func TestSet(t *testing.T) {
-	s, err := Set(when, lat, lon)
+	date, err := time.Parse(time.RFC3339, when)
+	if err != nil {
+		t.Fatal(err)
+	}
+	s, err := Set(date, lat, lon)
 	if err != nil {
 		t.Error(err)
 	}
-	if s.Truncate(time.Second) != set {
-		t.Errorf("expected %v, got %v", set, s)
+	ss, err := time.Parse(time.RFC3339, set)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s.Truncate(time.Second) != ss {
+		t.Errorf("expected %v, got %v", set, ss)
 	}
 }
