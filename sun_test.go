a18ce5957ae806ce14ddd8e4d9d9b308008a2434
Dimitri Sokolyuk <ds@doozer.de>
2016-11-17 19:35:19 +0100

    proper testing

--- sun_test.go
+++ sun_test.go
@@ -10,23 +10,35 @@ const (
 	lon = 13.4050
 )
 
+var (
+	when, _ = time.Parse(time.RFC3339, "2009-11-10T23:00:00Z")
+	rise, _ = time.Parse(time.RFC3339, "2009-11-10T06:18:22Z")
+	set, _  = time.Parse(time.RFC3339, "2009-11-10T15:21:24Z")
+)
+
 func TestDayOfYear(t *testing.T) {
-	d := dayOfYear(time.Now())
-	t.Log(d)
+	d := dayOfYear(when)
+	if d != 314 {
+		t.Errorf("expected 314, got %v", d)
+	}
 }
 
 func TestRise(t *testing.T) {
-	r, err := Rise(time.Now(), lat, lon)
+	r, err := Rise(when, lat, lon)
 	if err != nil {
 		t.Error(err)
 	}
-	t.Log(r)
+	if r.Truncate(time.Second) != rise {
+		t.Errorf("expected %v, got %v", rise, r)
+	}
 }
 
 func TestSet(t *testing.T) {
-	s, err := Set(time.Now(), lat, lon)
+	s, err := Set(when, lat, lon)
 	if err != nil {
 		t.Error(err)
 	}
-	t.Log(s)
+	if s.Truncate(time.Second) != set {
+		t.Errorf("expected %v, got %v", set, s)
+	}
 }
